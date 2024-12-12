92b000d79f2e77edd74fe8e3e463a4481b7e53a0
cinnamonwolfy <pocketlinux32@gmail.com>
2024-12-11 20:07:08 -0600

    the

--- src/login.c
+++ src/login.c
@@ -21,11 +21,17 @@ int plcu_login(plptr_t args, plmt_t* mt){
 	if(newlines[1] != NULL)
 		*(newlines[1]) = '\0';
 
-	printf("%s, %s\n", username, password);
-	if(plCUCheckPassword(username, password) == 0)
-		puts("yay! ^w^");
-	else
-		puts("aww... :<");
+	if(plCUCheckPassword(username, password) == 0){
+		struct passwd* userStruct = getpwnam(username);
+		setuid(userStruct->pw_uid);
+		setgid(userStruct->pw_gid);
+		chdir(userStruct->pw_dir);
+
+		//TODO: add invocation code
+		execv();
+	}else{
+		puts("Login incorrect");
+	}
 
 	return 0;
 }
